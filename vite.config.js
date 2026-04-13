6a71eab4ae6a5c6b5d48b37d491592adbdafbefa
dorus <dhakleijne@gmail.com>
2026-04-13 09:09:36 +0200

    testzoveel

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: './', // Zorgt dat paden relatief zijn voor GitHub Pages
 })

d18f70b567549b03320810983ec03498e566030f
dorus <dhakleijne@gmail.com>
2026-04-14 10:31:15 +0200

    switched to HashRouter for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link, useLocation, useNavigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link, useLocation, useNavigate } from "react-router-dom";
 import { DiscoProvider, useDisco } from "./context/DiscoContext";
 import Home from "./pages/Home";
 import Work from "./pages/Work";
